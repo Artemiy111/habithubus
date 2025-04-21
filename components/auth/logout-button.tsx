b96a1f67e0cdf1af5f4227dcd3bce629d51170b7
Artemiy111 <mrart111@mail.ru>
2025-04-21 16:01:28 +0300

    .

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -1,25 +1,26 @@
-'use client'
-
-import { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { authClient } from '@/lib/auth-client'
+import {auth} from '@/lib/auth'
+import { redirect } from 'next/navigation'
+import { headers } from 'next/headers'
 
-export default function LogoutButton() {
-  const [isLoading, setIsLoading] = useState(false)
+export default async function LogoutButton() {
   const handleLogout = async () => {
+    'use server'
     try {
-      setIsLoading(true)
+      await auth.api.signOut({headers: await headers()})
       await authClient.signOut()
+      redirect('/login')
     } catch (error) {
       console.error('Ошибка при выходе:', error)
-    } finally {
-      setIsLoading(false)
     }
   }
 
   return (
-    <Button variant="outline" onClick={handleLogout} disabled={isLoading} className="w-full">
-      {isLoading ? 'Выход...' : 'Выйти'}
-    </Button>
+    <form action={handleLogout} >
+      <Button variant="outline" className="w-full">
+        Выйти
+      </Button>
+      </form>
   )
 }
